241c9472e8f19402d771ff017c41e960ba3f1f82
David-mwas <dmwas704@gmaIl.com>
2024-10-28 15:40:20 +0300

    update service workers

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client"; // Change this line
 import "./index.css";
 import App from "./App";
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById("root")); // Use createRoot
 root.render(<App />); // Use the root to render your App
-// registerServiceWorker();
+registerServiceWorker();
