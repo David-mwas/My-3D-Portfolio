ad3b4f31135b87f58b61d15f632f3141c634673d
David-mwas <dmwas704@gmaIl.com>
2024-10-29 18:48:49 +0300

    Update ui,experience,resume

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,63 +1,63 @@
 import React from "react";
+import parse from "html-react-parser"; // Import parse
 
 const Resume = ({ data }) => {
   if (data) {
-    // var skillmessage = data.skillmessage;
-    var education = data.education.map(function (education) {
-      return (
-        <div key={education.school}>
-          <h3>{education.school}</h3>
-          <p className="info">
-            {education.degree} <span>&bull;</span>
-            <em className="date">{education.graduated}</em>
-          </p>
-          <p>{education.description}</p>
-        </div>
-      );
-    });
-    var work = data.work.map(function (work) {
-      return (
-        <div key={work.company}>
-          <h3>{work.company}</h3>
-          <p className="info">
-            {work.title}
-            <span>&bull;</span> <em className="date">{work.years}</em>
-          </p>
-          <p>{work.description}</p>
-        </div>
-      );
-    });
-   
-  }
-
-  return (
-    <section id="resume">
-      <div className="row work">
-        <div className="three columns header-col">
-          <h1>
-            <span>Experience</span>
-          </h1>
-        </div>
-
-        <div className="nine columns main-col">{work}</div>
+    const education = data.education.map((edu) => (
+      <div key={edu.school}>
+        <h3>{edu.school}</h3>
+        <p className="info">
+          {edu.degree} <span>&bull;</span>
+          <em className="date">{edu.graduated}</em>
+        </p>
+        <p>{edu.description}</p>
       </div>
+    ));
 
-      <div className="row education">
-        <div className="three columns header-col">
-          <h1>
-            <span>Education</span>
-          </h1>
-        </div>
+    const work = data.work.map((job, index) => (
+      <div key={job.company} className="work-item">
+        <div className="circle">{index + 1}</div> {/* Numbering */}
+        <h3>{job.company}</h3>
+        <p className="info">
+          {job.title}
+          <span>&bull;</span> <em className="date">{job.years}</em>
+        </p>
+        <p>{parse(job.description)}</p> {/* Wrap with parse */}
+      </div>
+    ));
 
-        <div className="nine columns main-col">
-          <div className="row item">
-            <div className="twelve columns">{education}</div>
+    return (
+      <section id="resume">
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1>
+              <span>Experience</span>
+            </h1>
+          </div>
+
+          <div className="nine columns main-col">
+            <div className="vertical-timeline md:right-[40px] ">{work}</div>
           </div>
         </div>
-      </div>
-     
-    </section>
-  );
+
+        <div className="row education">
+          <div className="three columns header-col">
+            <h1>
+              <span>Education</span>
+            </h1>
+          </div>
+
+          <div className="nine columns main-col">
+            <div className="row item">
+              <div className="twelve columns">{education}</div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+
+  return null; // Return null if no data is provided
 };
 
 export default Resume;
