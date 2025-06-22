ae36e281b56f592fc64406818407ae60d2110384
David-mwas <dmwas704@gmail.com>
2025-06-22 22:06:21 +0300

    Gsap animate Hero

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ProjectUpload from "./pages/ProjectUpload";
 import Login from "./pages/Login";
+import { SplitText } from "gsap/all";
+import gsap from "gsap";
 
 const App = () => {
+  gsap.registerPlugin(SplitText);
   return (
     <Router>
       <Routes>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,7 @@
-import { motion } from "framer-motion";
-// import TypeWriter from "react-typewriter";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
 import { FaReact, FaNodeJs } from "react-icons/fa";
 import {
   SiTypescript,
@@ -21,41 +23,130 @@ const techStack = [
 ];
 
 const Header = ({ data }) => {
-  const name = data?.name;
+  // const name = data?.name;
   const occupation = data?.occupation;
   const description = data?.description;
-  const city = data?.address.city;
-  const networks = data?.social.map((network, index) => (
-    <motion.li
-      key={network.name}
-      initial={{ y: 50, opacity: 0 }} // start from bottom with 0opacity
-      animate={{ y: 0, opacity: 1 }} // move up to its place
-      transition={{ duration: 0.5, delay: index * 0.2, ease: "easeOut" }} // stagger the animation
-    >
-      <a href={network.url}>
-        <i className={network.className}></i>
-      </a>
-    </motion.li>
-  ));
-
-  // Framer Motion variants for smooth entrance
-  const variantContainer = {
-    hidden: { opacity: 0 },
-    visible: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.2,
-      },
+  const city = data?.address?.city;
+  const social = [
+    {
+      name: "github",
+      url: "https://github.com/David-mwas",
+      className: "fa fa-github",
     },
-  };
+    {
+      name: "gtwitter",
+      url: "https://x.com/DavidMwas2_0",
+      className: "fa fa-twitter",
+    },
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/david-mwangi-a57186235/",
+      className: "fa fa-linkedin",
+    },
+  ];
 
-  const variantItem = {
-    hidden: { opacity: 0, y: 50 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
-  };
+  const containerRef = useRef();
+  const nameRef = useRef();
+  const textRef = useRef();
+  const imageRef = useRef();
+  const badgeRefs = useRef([]);
+  const socialRefs = useRef([]);
+
+  useGSAP(
+    () => {
+      // Animate image
+      gsap.fromTo(
+        imageRef.current,
+        { scale: 0, opacity: 0 },
+        { scale: 1, opacity: 1, duration: 1, ease: "back.out(1.7)" }
+      );
+
+      // SplitText animation
+      const splitText = new SplitText(nameRef.current, {
+        type: "chars, words",
+      });
+
+      console.log(splitText);
+
+      gsap.from(splitText.chars, {
+        y: 100,
+        autoAlpha: 0,
+        rotate: gsap.utils.wrap([-90, 90]),
+        duration: 1,
+        stagger: 0.05,
+        ease: "back.out(1.7)",
+      });
+
+      // Description animation
+      gsap.fromTo(
+        textRef.current,
+        { y: 50, opacity: 0 },
+        { y: 0, opacity: 1, duration: 1, delay: 0.8, ease: "power3.out" }
+      );
+
+      // Tech badges
+      badgeRefs.current.forEach((el, i) => {
+        gsap.fromTo(
+          el,
+          {
+            opacity: 0,
+            scale: 0,
+            rotation: gsap.utils.random(-360, 360),
+            x: gsap.utils.random(-300, 300),
+            y: gsap.utils.random(-200, 200),
+          },
+          {
+            opacity: 1,
+            scale: 1,
+            rotation: 0,
+            x: 0,
+            y: 0,
+            duration: 1,
+            ease: "elastic.out(1, 0.3)",
+            delay: i * 0.06,
+          }
+        );
+
+        // Add hover effect
+        el.addEventListener("mouseenter", () => {
+          gsap.to(el, {
+            scale: 0.8,
+            rotate: gsap.utils.random(-10, 10),
+            duration: 0.01,
+            ease: "power2.out",
+          });
+        });
+
+        el.addEventListener("mouseleave", () => {
+          gsap.to(el, {
+            scale: 1,
+            rotate: 0,
+            duration: 0.01,
+            ease: "power2.inOut",
+          });
+        });
+      });
+
+      // Socials
+      socialRefs.current.forEach((el, i) => {
+        gsap.fromTo(
+          el,
+          { y: 50, opacity: 0 },
+          {
+            y: 0,
+            opacity: 1,
+            duration: 0.6,
+            delay: i * 0.2 + 1,
+            ease: "back.out(1.7)",
+          }
+        );
+      });
+    },
+    { scpe: containerRef }
+  );
 
   return (
-    <header id="home">
+    <header id="home" ref={containerRef}>
       <nav id="nav-wrap">
         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
           Show navigation
@@ -97,78 +188,63 @@ const Header = ({ data }) => {
           </li>
         </ul>
       </nav>
-
-      <div className="row banner">
-        <motion.div
-          initial={{ scale: 0.8, opacity: 0 }}
-          animate={{ scale: 1, opacity: 1 }}
-          transition={{ duration: 0.7, ease: "easeOut" }}
+      <div className="row banner flex flex-col items-center justify-center text-center">
+        <div
+          ref={imageRef}
           className="bg-top rounded-full bg-no-repeat shadow-custom-blueviolet order-1 justify-self-center bg-cover w-[160px] h-[150px] md:w-[260px] md:h-[250px] p-1 animate-profile-animate"
           style={{ backgroundImage: `url(/images/mwas.webp)` }}
-        >
-          {/* <img src="/images/mwas.jpg" alt="mwas pic" id="mwas" />*/}
-        </motion.div>
-        <div className="banner-text">
-          {/* Technologies Display with Icon Logos and Colors */}
-          {/* Banner text with smooth upward entrance */}
-          <motion.div
-            initial={{ y: 50, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.7, ease: "easeOut" }}
-          >
-            <h1 className="responsive-headline w-full">I'm {name}.</h1>
-            <h3>
-              Based in {city}, Kenya <br />
-              <a
-                rel="noreferrer"
-                href="https://ihub.mmust.ac.ke/about"
-                id="occupation"
-                target="_blank"
-              >
-                {occupation}
-              </a>
-              . {description}
-            </h3>
+        />
 
-            {/* Animated badges with stagger effects */}
-            <motion.div
-              variants={variantContainer}
-              initial="hidden"
-              animate="visible"
-              className="tech-container flex flex-wrap gap-4 mt-4 w-full items-center justify-center"
+        <div className="banner-text mt-6">
+          <h1 ref={nameRef} className="responsive-headline w-full">
+            I'm David Mwangi Wanjiru
+          </h1>
+
+          <h3 ref={textRef}>
+            Based in {city}, Kenya <br />
+            <a
+              rel="noreferrer"
+              href="https://ihub.mmust.ac.ke/about"
+              id="occupation"
+              target="_blank"
             >
-              {techStack.map((tech, index) => (
-                <motion.div
-                  key={index}
-                  variants={variantItem}
-                  whileHover={{
-                    scale: 1.1,
-                    boxShadow: "0px 4px 20px rgb(0,0,0,0.5)",
-                  }}
-                  className="badge flex items-center font-[500] px-4 py-2 rounded-md text-base transition duration-300 ease-in-out uppercase bg-[blueviolet] text-white"
+              {occupation}
+            </a>
+            . {description}
+          </h3>
+          <div className="tech-container flex flex-wrap gap-4 mt-6 w-full items-center justify-center">
+            {techStack?.map((tech, index) => (
+              <div
+                key={index}
+                ref={(el) => (badgeRefs.current[index] = el)}
+                className="badge flex items-center font-[500] px-4 py-2 rounded-md text-base transition duration-300 ease-in-out uppercase bg-[blueviolet] text-white"
+              >
+                <span
+                  className="mr-2 text-3xl text-center shadow-xl shadow-black"
+                  style={{ color: tech.color }}
                 >
-                  <span
-                    className="mr-2 text-3xl text-center shadow-xl shadow-black"
-                    style={{ color: tech.color }}
-                  >
-                    {tech.icon}
-                  </span>
-                  <span>{tech.name}</span>
-                </motion.div>
-              ))}
-            </motion.div>
-          </motion.div>
+                  {tech.icon}
+                </span>
+                <span>{tech.name}</span>
+              </div>
+            ))}
+          </div>
 
-          <hr />
-          <ul className="social">{networks}</ul>
+          <hr className="my-6" />
+          <ul className="social flex gap-4 justify-center mt-2">
+            {social?.map((network, index) => (
+              <li
+                key={network.name}
+                ref={(el) => (socialRefs.current[index] = el)}
+              >
+                <a href={network.url} target="_blank" rel="noreferrer">
+                  <i className={`${network.className} text-5xl`}></i>
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
-
-      {/* <p className="scrolldown">
-        <a className="smoothscroll" href="#about">
-          <i className="icon-down-circle"></i>
-        </a>
-      </p> */}
     </header>
   );
 };
